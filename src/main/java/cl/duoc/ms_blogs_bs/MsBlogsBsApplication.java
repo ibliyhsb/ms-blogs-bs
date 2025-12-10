d9a38417c184c2963b9dcefd13c075bfe80d66c3
ibliyhsb <catd.rojas@duocuc.cl>
2025-12-10 18:26:32 -0300

    ms-blogs-bs

--- src/main/java/cl/duoc/ms_blogs_bs/MsBlogsBsApplication.java
+++ src/main/java/cl/duoc/ms_blogs_bs/MsBlogsBsApplication.java
@@ -8,19 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(basePackages = "cl.duoc.ms_blogs_bs.client")
 public class MsBlogsBsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MsBlogsBsApplication.class, args);
-    }
-
-}
-package cl.duoc.ms_blogs_bs;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MsBlogsBsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsBlogsBsApplication.class, args);
 	}
