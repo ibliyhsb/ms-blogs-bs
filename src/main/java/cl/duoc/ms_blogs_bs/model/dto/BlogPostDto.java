eacac1416c3d387b450c2a485bda587a3755d166
Alemstar <al.faguas@duocuc.cl>
2025-12-15 22:44:47 -0300

    modificaciones en microservicio de blogs bs

--- src/main/java/cl/duoc/ms_blogs_bs/model/dto/BlogPostDto.java
+++ src/main/java/cl/duoc/ms_blogs_bs/model/dto/BlogPostDto.java
@@ -1,5 +1,6 @@
 package cl.duoc.ms_blogs_bs.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,12 +9,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BlogPostDto {
+    @JsonProperty("id")
     private Long id;
+    
+    @JsonProperty("titulo")
     private String titulo;
+    
+    @JsonProperty("descripcion")
     private String descripcion;
+    
+    @JsonProperty("contenido")
     private String contenido;
+    
+    @JsonProperty("usuarioAutor")
     private String usuarioAutor;
+    
+    @JsonProperty("fechaPublicacion")
     private String fechaPublicacion;
+    
+    @JsonProperty("esPublicado")
     private Boolean esPublicado;
+    
+    @JsonProperty("imagenUri")
     private String imagenUri;
 }
